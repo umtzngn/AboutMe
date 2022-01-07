4c06c4a3098aee18b94892cb4458d716085721b9
Umut Zengin <umutzengin@gmail.com>
2022-01-07 08:55:25 +0300

    Keyboard visibility added

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethod
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,6 +17,10 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
+        }
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
         }
 
 
@@ -30,6 +37,29 @@ class MainActivity : AppCompatActivity() {
         view.visibility=View.GONE
 
         nicknameTextView.visibility=View.VISIBLE
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+
+
+    }
+
+
+    private  fun updateNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+
+        view.visibility = View.GONE
+
+        editText.requestFocus()
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(editText,0)
+
+
 
     }
 }
